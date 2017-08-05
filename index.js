6af4a712923d6f3bc051667a07b9c1e4e7c0f365
peterchic <peterchicarielli@gmail.com>
2017-08-05 13:26:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,44 @@
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(currentNode, newNode){
+  if(currentNode.data === newNode.data){
+    return true
+  }
+  if(newNode.data < currentNode.data){
+      if(currentNode.left){
+        return findOrAdd(currentNode.left, newNode)
+      } else {
+        currentNode.left = newNode
+      }
+  } else if(newNode.data > currentNode.data) {
+    if(currentNode.right){
+      return findOrAdd(currentNode.right, newNode)
+    } else {
+      currentNode.right = newNode
+    }
+  }
+}
+
+let max = (currentNode) => {
+  if(currentNode.right){
+    return max(currentNode.right)
+  } else {
+    return currentNode
+  }
+}
+
+let min = (currentNode) => {
+  if(currentNode.left){
+    return min(currentNode.left)
+  } else {
+    return currentNode
+  }
+}
